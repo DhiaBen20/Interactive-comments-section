154a173eb2cd71ceead6927da8ceb105551d233a
Dhia Bendjedia <dhia.bendjedia20@gmail.com>
2023-06-01 05:35:50 +0100

    Fix accessibility issues

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -39,39 +39,45 @@ function CommentForm({ onSubmit, replyingTo = "", prevContent = "" }) {
     }`;
 
     return (
-        <Card noPadding={!!prevContent}>
-            <form onSubmit={handleSubmit} className={formStyles}>
-                {!prevContent && (
-                    <div className={`w-9 hidden md:block`}>
-                        <Avatar />
-                    </div>
-                )}
-
-                <div className="flex-1">
-                    <Textarea
-                        name="comment"
-                        placeholder="Add a comment..."
-                        defaultValue={defaultValue}
-                    />
-                </div>
-
-                <div
-                    className={`flex items-center ${
-                        !prevContent ? "justify-between" : "justify-end"
-                    } mt-2 md:mt-0`}
-                >
+        <article>
+            <Card noPadding={!!prevContent}>
+                <form onSubmit={handleSubmit} className={formStyles}>
                     {!prevContent && (
-                        <div className={`w-9 md:hidden`}>
+                        <div className={`w-9 hidden md:block`}>
                             <Avatar />
                         </div>
                     )}
 
-                    <Button loading={loading}>
-                        {prevContent ? "Update" : replyingTo ? "Reply" : "Send"}
-                    </Button>
-                </div>
-            </form>
-        </Card>
+                    <div className="flex-1">
+                        <Textarea
+                            name="comment"
+                            placeholder="Add a comment..."
+                            defaultValue={defaultValue}
+                        />
+                    </div>
+
+                    <div
+                        className={`flex items-center ${
+                            !prevContent ? "justify-between" : "justify-end"
+                        } mt-2 md:mt-0`}
+                    >
+                        {!prevContent && (
+                            <div className={`w-9 md:hidden`}>
+                                <Avatar />
+                            </div>
+                        )}
+
+                        <Button loading={loading}>
+                            {prevContent
+                                ? "Update"
+                                : replyingTo
+                                ? "Reply"
+                                : "Send"}
+                        </Button>
+                    </div>
+                </form>
+            </Card>
+        </article>
     );
 }
 
