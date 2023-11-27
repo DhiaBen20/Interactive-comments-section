e6c4d0c0e22b701f66c06330b22c2f18f8e8259a
dhia <dhia.bendjedia20@gmail.com>
2023-11-27 18:40:49 +0100

    Add and configure typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
         "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime"
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended",
     ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    ignorePatterns: ["dist", ".eslintrc.cjs"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["react-refresh"],
+    rules: {
+        "react-refresh/only-export-components": [
+            "warn",
+            { allowConstantExport: true },
+        ]
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+};
